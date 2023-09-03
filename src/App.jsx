8a6678317704dd27d3cdaa41d60ca2eb1a6f6824
khushwantD <101801251+khushwantD@users.noreply.github.com>
2023-09-03 14:22:54 +0530

    small tweeks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar"
-import WeatherDashboard from "./weatherApp"
+import WeatherDashboard from "./components/weatherApp"
 
 function App() {
   
--- src/ContextPage.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useState } from "react";
-
-const ContextPage = createContext('')
-
-export const WeatherDataProvider = ({ children }) => {
-    
-}
